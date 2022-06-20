8201039ac14fd3880d96f35fc7e5109b79fc054f
Juan Ignacio <zalbak87@gmail.com>
2022-06-19 21:45:53 -0300

    Clase 4

---
+++ operadores_variables.py
@@ -0,0 +1,27 @@
+#Variable es un espacio de memoria del ordenador donde se almacenará un valor que podrá cambiar durante la ejecucion del programa
+#No debemos definir de que tipo es la variable se define por lo que contiene la misma no debemos poner int numero=5 solo numero=5
+#Python es un lenguaje 100% orientado a objetos, para python una variable es un objeto
+#Operadores
+a=5
+b=4
+suma=a+b
+print(suma)
+#Operador modulo es el resto de una divicion
+modulo=10%3
+print(modulo)
+#Exponente
+exponente=5**3
+print(exponente)
+#Divicion entera(no muestra el decimal)
+div=9//2
+print(div)
+#Función type() nos devuelve el tipo de dato de una variable, o el tipo de variable
+nombre="juan"
+type(nombre)
+print(type(nombre))
+#Si usamos tres comillas nos sirve para los saltos de linea
+mensaje="""ESTO ES UN MENSAJE
+CON TRES SALTOS 
+DE LINEA"""
+print(mensaje)
+#El operador = es el operador de asignación en cambio el == es el operador de comparación (si9 dos valores son iguales)
