07c8639268cc566eff10410c1c4069df6db61579
Juan Ignacio <zalbak87@gmail.com>
2022-06-19 22:11:06 -0300

    Sintaxis de las funciones

---
+++ funciones1.py
@@ -0,0 +1,29 @@
+#Conjunto de líneas de código agrupadas(bloque de código) que funcionan como una unidad realizando una tarea específica.
+#Las funciones pueden devolver valores.
+#Pueden tener parametros o argumentos.
+#Tambien se denominan "métodos" cuando se encuentran definidas dentro de una clase.
+
+#Utilidad
+#Reutilizar codigo.
+
+#Sintaxis
+#def nombre_funcion():
+    #instrucciones de la funcion
+    #return # esto es opcional
+
+#def nombre_funcion_parametros(parametros):
+    #instrucciones de la funcion
+    #return #esto es opcional
+
+#Ejecución
+#nombre_funcion()
+#nombre_funcion_parametros("parametro")
+
+#Tenemos funciones predefinidas, que son las de python y propias que son las que creamos
+#print("Estamos aprendiendo python")
+
+def imprime():
+    print("Estamos aprendiendo python")
+    
+#La función no hace nada si no la llamamos
+imprime()
