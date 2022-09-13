999836c9f8a1251907b61ff10eb424d79faa2964
Juan Ignacio <zalbak87@gmail.com>
2022-09-13 19:11:02 -0300

    Ultimo video paquetes

---
+++ stup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+
+    name="paquetecalculos",
+    version="1.0",
+    description="Paquete de redondeo y potencia",
+    author="Juan Ignacio Alonso Flieller",
+    author_email="zalbak87@gmail.com",
+    packages=["Calculos","Calculos.redondeo_potencia"]
+)
